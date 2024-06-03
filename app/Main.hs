cb7b2155fb12a746db83c9f71e0b91bea7dc2508
Karl Ostmo <kostmo@gmail.com>
2024-06-02 17:14:05 -0700

    add some triangles

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,14 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Path.Metafont
 
+-- Vaporwave colors
+-- https://www.color-hex.com/color-palette/57915
+fuschiaColor = sRGB24read "#ff00c1"
+purpleColor = sRGB24read "#9600ff"
+violetColor = sRGB24read "#4900ff"
+blueColor = sRGB24read "#00b8ff"
+cyanColor = sRGB24read "#00fff9"
+
 sShape :: Diagram B
 sShape = fromVertices $ map p2 [
       (0, 0)
@@ -33,36 +41,47 @@ rShape = fromVertices $ map p2 [
     ]
 
 lambdaChar :: Diagram B
-lambdaChar = straightBranch `atop` curvedPart
+lambdaChar = centerX lineShapes `atop` deltaShape
   where
-  straightBranch = fromVertices $ [
+  lineShapes = straightBranch `atop` curvedPart
+  deltaShape = alignB $ triangleSequence 1
+
+  straightBranch = fromVertices [
       p2 (0, 0)
     , apexPt
     ]
-  curvedPart = metafont $ bottomRightPt .--. apexPt .--.endpt (p2 (0, 3))
+  curvedPart = metafont $
+    bottomRightPt .--. apexPt .--. endpt (p2 (0, 3))
+
   bottomRightPt = p2 (2, 0)
   apexPt = p2 (1, 2)
 
-composed :: Diagram B
-composed = hsep 0.2 $ map alignB [
+composedLetters :: Diagram B
+composedLetters = hsep 0.5 $ map alignB [
     sShape # lc blueColor
   , reflectY $ mShape # lc fuschiaColor
-  , lambdaChar # lineCap LineCapButt # lw 10 # lc purpleColor
+  , lambdaChar # lineCap LineCapButt # lw 15 # lc purpleColor
   , rShape # lc blueColor
   , mShape # lc fuschiaColor
   ]
+
+triangleSequence :: Int -> Diagram B
+triangleSequence count = hsep 0.5 $ replicate count mkTriangle
   where
-    -- Vaporwave colors
-    -- https://www.color-hex.com/color-palette/57915
-    fuschiaColor = sRGB24read "#ff00c1"
-    purpleColor = sRGB24read "#9600ff"
-    violetColor = sRGB24read "#4900ff"
-    blueColor = sRGB24read "#00b8ff"
-    cyanColor = sRGB24read "#00fff9"
+  mkTriangle = rotateBy (-(1/4)) $
+    triangle 0.6 # lw 0 # fc cyanColor
+
+fullDiagram :: Diagram B
+fullDiagram = mconcat [
+    alignR $ alignT (triangleSequence 3) `atop` alignT strokedLetters
+  , alignR $ alignT (triangleSequence 4)
+  ]
+  where
+  strokedLetters = composedLetters
+    # lineCap LineCapSquare . lineJoin LineJoinBevel
+    # lw 15
+    # frame 0.2
 
 main :: IO ()
-main = mainWith $ composed
-    # lineCap LineCapSquare . lineJoin LineJoinBevel
-    # lw 5
-    # frame 0.2
+main = mainWith $ fullDiagram # frame 0.2
   
